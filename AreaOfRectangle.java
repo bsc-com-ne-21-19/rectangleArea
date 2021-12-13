6d8de38c97d67b6536716a8c9a6b1bb5da0f77b4
bsc-com-ne-21-19 <bsc-com-ne-21-19@unima.ac.mw>
2021-12-13 07:38:13 +0200

    added license file

---
+++ AreaOfRectangle.java
@@ -0,0 +1,11 @@
+/**
+ * @author BSC-COM-NE-21-19 This program calculates area of a rectangle
+ */
+
+public class AreaOfRectangle {
+
+    // main method
+    public static void main(String[] args) {
+        System.out.println("The area will be shown here");
+    }
+}
